25e89d53376ad8b5314154dd1f9f26848885654a
edu1107zera <128429561+edu1107zera@users.noreply.github.com>
2023-07-21 21:08:12 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace estoque_top
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnEstacionar = new System.Windows.Forms.Button();
+            this.btnRetirar = new System.Windows.Forms.Button();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvLista = new System.Windows.Forms.ListView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblVeiculos = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPlaca);
+            this.groupBox1.Controls.Add(this.btnRetirar);
+            this.groupBox1.Controls.Add(this.btnEstacionar);
+            this.groupBox1.Controls.Add(this.btnFechar);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(40, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 233);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operaçoes";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(9, 31);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(133, 40);
+            this.btnFechar.TabIndex = 0;
+            this.btnFechar.Text = "Fechar estacionamento";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // btnEstacionar
+            // 
+            this.btnEstacionar.Location = new System.Drawing.Point(9, 77);
+            this.btnEstacionar.Name = "btnEstacionar";
+            this.btnEstacionar.Size = new System.Drawing.Size(90, 40);
+            this.btnEstacionar.TabIndex = 1;
+            this.btnEstacionar.Text = "Estacionar";
+            this.btnEstacionar.UseVisualStyleBackColor = true;
+            this.btnEstacionar.Click += new System.EventHandler(this.btnEstacionar_Click);
+            // 
+            // btnRetirar
+            // 
+            this.btnRetirar.Location = new System.Drawing.Point(9, 123);
+            this.btnRetirar.Name = "btnRetirar";
+            this.btnRetirar.Size = new System.Drawing.Size(71, 40);
+            this.btnRetirar.TabIndex = 2;
+            this.btnRetirar.Text = "Retirar";
+            this.btnRetirar.UseVisualStyleBackColor = true;
+            this.btnRetirar.Click += new System.EventHandler(this.btnRetirar_Click);
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(0, 204);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(160, 21);
+            this.txtPlaca.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Digite a placa:";
+            // 
+            // lvLista
+            // 
+            this.lvLista.HideSelection = false;
+            this.lvLista.Location = new System.Drawing.Point(6, 77);
+            this.lvLista.Name = "lvLista";
+            this.lvLista.Size = new System.Drawing.Size(175, 149);
+            this.lvLista.TabIndex = 1;
+            this.lvLista.UseCompatibleStateImageBehavior = false;
+            this.lvLista.View = System.Windows.Forms.View.List;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblVeiculos);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.lvLista);
+            this.groupBox2.Location = new System.Drawing.Point(227, 46);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(187, 233);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Veiculos:";
+            // 
+            // lblVeiculos
+            // 
+            this.lblVeiculos.AutoSize = true;
+            this.lblVeiculos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVeiculos.Location = new System.Drawing.Point(96, 42);
+            this.lblVeiculos.Name = "lblVeiculos";
+            this.lblVeiculos.Size = new System.Drawing.Size(17, 18);
+            this.lblVeiculos.TabIndex = 6;
+            this.lblVeiculos.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(157, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Estacionamento";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 372);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.Button btnRetirar;
+        private System.Windows.Forms.Button btnEstacionar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.ListView lvLista;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblVeiculos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace estoque_top
+{
+    public partial class Form1 : Form
+    {
+        List<string> Estacionamento = new List<string>();//Lista
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void verifcListCHEIA()//FUNÇAO PARA VERIFICAR SE A LISTA/ESTACIONAMENTO ESTA CHEIA(O)
+        {
+            /**
+             cria um if para que veja se a lista  esta dentro do limite de itens estabelecido
+             o estacionamento tem um limite de 5 carros ent no if colocamos o nome da list e .Count
+             para que seja contado quantos itens a lista tem e fazemos isso com uma determinaçao >= a 5
+             para que essa contagem tenha um limite de itens ate 5 e pare apos isso usamos o return para que 
+             ao parar ela feche
+            ------------------------------------------------------------------------------------------------
+            if=SE
+            se estacionamento for maior ou igual a 5 mostrar mensagem(MessageBox.Show) e fechar(return).
+             **/
+          
+            //verifica se ta cheio o estacionamento-------------------------------------------/
+            if (Estacionamento.Count == 5)
+            {
+                MessageBox.Show("Estacionamento cheio");
+                return;
+                
+            }
+           
+        }
+                
+               
+       
+        void verific_SE_a_TXTboxESTAvazia()
+        {
+            /**
+             if=SE
+            se a caixa de texto(txtPlaca) for igual a vazio("") 
+            mostrar mensagem(MessageBox.Show) e fechar(return).
+             */
+
+            // verifica se a pessoa digitou alg----------------------------/
+            if (txtPlaca.Text == "")
+            {
+                MessageBox.Show("Preencha todos os campos");
+                return;
+            }
+        }
+        
+        void adicionaPlaca()
+        {
+            //adiciona placa na lista do codigo
+            string placa = txtPlaca.Text;
+            Estacionamento.Add(placa);
+        }
+
+        void ATUALIZA_interface_e_labe_DE_CONTAGEM()
+        {
+            /** 
+             pegamos o texto da lista e recebemos nele a contagem da lista
+           
+            lblVeiculos.text(o .Text foi usado para identificar o que queremos da label)
+            Estacionamento.Count.ToString()(count foi usado para contar os elementos da lista 
+            estacionamento e o ToString para que me mostre essa contagem em string.
+             */
+            //atualiza o label de contagem-----------------------------------------------/
+            lblVeiculos.Text = Estacionamento.Count.ToString();
+
+          
+           
+           /**
+            LvLista.Items(ListView tem como diferença 
+            o uso do Items para denominar oque na ListView 
+            foi especificado(chamado/usado))
+           
+            lvList.Items.Clear(); (apaga os itens da ListView para atuar como 
+            um reset funcionand assim uma nova leitura e nao acontecendo erro de contagem.) 
+            */
+            //atualiza a lista da interface---------------------------------------------------------/
+            lvLista.Items.Clear();
+            for (int i = 0; i < Estacionamento.Count; i++)
+            {
+
+                lvLista.Items.Add($"[VAGA {i}] - {Estacionamento[i]}");
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEstacionar_Click(object sender, EventArgs e)//BOTAO ESTACIONAR
+        {
+            
+           
+            
+            verific_SE_a_TXTboxESTAvazia();
+            adicionaPlaca();
+            ATUALIZA_interface_e_labe_DE_CONTAGEM();
+            verifcListCHEIA();
+            
+
+
+
+
+
+        }
+
+        private void btnRetirar_Click(object sender, EventArgs e)//Botao de retirar o veiculo
+        {
+            /**
+            Funciona igual ao botao de adicioar 
+            com o diferencial que ao contrario de add ele usa remove 
+            tendo as mesmas funçoes de atualizaçao para mostrar na tela etc.
+             */
+            //REMOVER
+            string placa = txtPlaca.Text;
+            Estacionamento.Remove(placa);
+            //atualiza o label de contagem
+            lblVeiculos.Text = Estacionamento.Count.ToString();
+
+            //atualiza a lista da interface
+            lvLista.Items.Clear();
+            for (int i = 0; i < Estacionamento.Count; i++)
+            {
+
+                lvLista.Items.Add($"[VAGA {i}] - {Estacionamento[i]}");
+            }
+
+        }
+    }
+}
+            
+          
+            
+
+            
+          
+
+
+
+            
+
+
+
+
+
+
